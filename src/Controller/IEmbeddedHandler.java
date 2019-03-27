09853f42ffc4e1c833b54e60eed3f583d924a71f
kennethp <kennethpete@gmail.com>
2019-03-27 14:44:34 +0100

    Interface for embeddedHandler

---
+++ src/Controller/IEmbeddedHandler.java
@@ -0,0 +1,8 @@
+package Controller;
+
+public interface IEmbeddedHandler {
+	public void waterPlant(int amount);
+	public float getHumidity();
+	public float getTemperature();
+	public float getCO2();
+}
