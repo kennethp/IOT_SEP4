e6da9edf22a3d5a1d18fca2d7ba77336b9fd16c3
kenneth <kennethpete@gmail.com>
2019-04-22 16:13:07 +0200

    Changed to real database

--- src/presenter/Server.java
+++ src/presenter/Server.java
@@ -1,5 +1,7 @@
 package presenter;
 
+import model.User;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -12,14 +14,9 @@ public class Server {
 	static IDatabaseHandler databaseHandler;
 
 	public static void main(String[] args) {
-		String username = null;
-		try {
-			username = URLEncoder.encode("kennethpete@gmail.com", "UTF-8");
-		} catch(UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		String password = "12345678X!";
 		databaseHandler = new DatabaseHandler(
-				"mongodb+srv://" + username + ":" + password + "@cluster0-7fj2z.mongodb.net/test?retryWrites=true");
+				"mongodb+srv://Llamaxy:865feeBA@test-i10mg.mongodb.net/test?retryWrites=true");
+
+
 	}
 }
