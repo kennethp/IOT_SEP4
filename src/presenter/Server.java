62f5cd087d1238e00e0d020a2e1e6c7ac5efa87d
kenneth <kennethpete@gmail.com>
2019-04-25 04:16:46 +0200

    Real server implementation

--- src/presenter/Server.java
+++ src/presenter/Server.java
@@ -5,18 +5,27 @@ import model.User;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Server {
-	static String host = "mongodb+srv://cluster0-7fj2z.mongodb.net";
-	static String user = "test";
-	static String pass = "plant123";
+	static String host = "@test-i10mg.mongodb.net/test?retryWrites=true";
+	static String user = "Llamaxy";
+	static String pass = "865feeBA";
 
-	static IDatabaseHandler databaseHandler;
+	IDatabaseHandler databaseHandler;
+	WebserviceConnector webserviceConnector;
+	IWebserviceHandler webserviceHandler;
 
-	public static void main(String[] args) {
-		databaseHandler = new DatabaseHandler(
-				"mongodb+srv://Llamaxy:865feeBA@test-i10mg.mongodb.net/test?retryWrites=true");
+	ExecutorService executorService;
 
+	public Server() {
+		executorService = new ScheduledThreadPoolExecutor(2);
 
+		databaseHandler = new DatabaseHandler("mongodb+srv://" + user + ':' + pass + host);
+		webserviceHandler = new WebserviceHandler(databaseHandler);
+		webserviceConnector = new WebserviceConnector(webserviceHandler, 3001);
+		executorService.submit(webserviceConnector);
 	}
 }
