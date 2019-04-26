8d96cec92ca43bbfbc1971a62933c787bbfdf086
kenneth <kennethpete@gmail.com>
2019-04-26 14:37:21 +0200

    Socket connection test client

---
+++ src/test/Client.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+	Socket socket;
+
+	public Client(Socket socket) {
+		this.socket = socket;
+	}
+
+	public void write(String input) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+		bw.write(input + '\0');
+		bw.flush();
+		System.out.println("Client: Input written");
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		int c;
+		int n = 0;
+		while((c = br.read()) != 0) {
+			if(c == -1) {
+				break;
+			}
+			System.out.println(">Client: " + n++ + " characters read from stream");
+			System.out.println();
+			baos.write(c);
+		}
+		String response = new String(baos.toByteArray(), "UTF-8");
+		System.out.println(response);
+	}
+}
