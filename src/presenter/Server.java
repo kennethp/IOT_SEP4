a72195f0b85e7c270db6dcc40a36ea742b1b86f1
kenneth <kennethpete@gmail.com>
2019-05-09 14:19:47 +0200

    Print method added

--- src/presenter/Server.java
+++ src/presenter/Server.java
@@ -1,22 +1,21 @@
 package presenter;
 
-import model.User;
+import model.Account;
+import model.PlantProfile;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class Server {
 	static String host = "@test-i10mg.mongodb.net/test?retryWrites=true";
 	static String user = "Llamaxy";
 	static String pass = "865feeBA";
 
-	IDatabaseHandler databaseHandler;
-	WebserviceConnector webserviceConnector;
-	IWebserviceHandler webserviceHandler;
+	public IDatabaseHandler databaseHandler;
+	public WebserviceConnector webserviceConnector;
+	public IWebserviceHandler webserviceHandler;
 
 	public static ExecutorService executorService;
 
@@ -33,5 +32,17 @@ public class Server {
 		return webserviceConnector.STATUS
 				+ '\n'
 				+ databaseHandler.getStatus();
+	}
+
+	public List<String> printPlants() {
+		ArrayList<String> list = new ArrayList<>();
+
+		PlantProfile p;
+		int i = 1;
+		while((p = databaseHandler.getPlantProfile(i)) != null) {
+			list.add(p.toJson());
+		}
+
+		return list;
 	}
 }
