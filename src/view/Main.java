71893c44d833840799f845a128bd2cc148375051
kenneth <kennethpete@gmail.com>
2019-05-09 14:16:45 +0200

    CLI updated

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,7 @@
 package view;
 
+import model.Account;
+import model.PlantProfile;
 import presenter.Server;
 
 import java.util.Scanner;
@@ -18,8 +20,33 @@ public class Main {
 					System.out.println("Commands: ");
 					System.out.println(" help");
 					System.out.println(" status");
+					System.out.println(" getusers");
+					System.out.println(" getplant");
 					System.out.println(" exit");
 					break;
+				case "getplant":
+					System.out.println("Enter id:");
+					String ids = scanner.nextLine();
+					int id;
+					try {
+						id = Integer.parseInt(ids);
+					} catch(NumberFormatException e) {
+						System.out.println("Invalid id");
+						break;
+					}
+
+					PlantProfile p = server.databaseHandler.getPlantProfile(id);
+					System.out.println(p.toJson());
+					break;
+
+				case "getaccount":
+					System.out.println("Enter id");
+					String accountIds = scanner.nextLine();
+
+					Account acc = server.databaseHandler.getAccount(accountIds);
+					System.out.println(acc.toJson());
+					break;
+
 				case "status":
 					System.out.println(server.getStatus());
 					break;
