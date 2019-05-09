09ab77921d99127acf321a425679d3ef9ed7b044
kenneth <kennethpete@gmail.com>
2019-05-09 14:14:35 +0200

    Updated to fit current models

--- src/presenter/DatabaseHandler.java
+++ src/presenter/DatabaseHandler.java
@@ -1,26 +1,17 @@
 package presenter;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.MongoException;
-import com.mongodb.MongoWriteConcernException;
-import com.mongodb.MongoWriteException;
 import com.mongodb.client.*;
-import model.Plant;
-import model.PlantMonitor;
-import model.User;
-import org.bson.BsonDocument;
-import org.bson.BsonInt32;
+import model.Account;
+import model.PlantProfile;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-
-import javax.print.Doc;
-import java.util.function.Consumer;
 
 
 public class DatabaseHandler implements IDatabaseHandler {
 	MongoClient mongo;
 	MongoDatabase database;
-	MongoCollection<Document> userCollection;
+	MongoCollection<Document> accountCollection;
 	MongoCollection<Document> plantCollection;
 	public String STATUS = "Not connected";
 
@@ -35,9 +26,8 @@ public class DatabaseHandler implements IDatabaseHandler {
 			STATUS = "Connected to mongoDB";
 		}
 
-		plantCollection = database.getCollection("Plants");
-
-//		userCollection = database.getCollection("Test");
+		plantCollection = database.getCollection("PlantProfiles");
+		accountCollection = database.getCollection("Accounts");
 	}
 
 	@Override
@@ -45,79 +35,59 @@ public class DatabaseHandler implements IDatabaseHandler {
 		return STATUS;
 	}
 
-	/**
-	 * Returns a User fetched from database
-	 * @param id Users id in database
-	 * @return User object
-	 */
-
-
 	@Override
-	public User getUser(int id) {
-		Bson filter = new BasicDBObject("UserId", id);
-		Document document = userCollection.find(filter).first();
-		return User.fromJson(document.toJson());
+	public Account getAccount(String user) {
+		Bson filter = new BasicDBObject("Username", user);
+		Document document = accountCollection.find(filter).first();
+		document.remove("_id");
+		return Account.fromJson(document.toJson());
 	}
 
 	@Override
-	public boolean setUser(User user) {
-		return set(new BasicDBObject(), Document.parse(user.toJson()), userCollection);
-	}
-
-	@Override
-	public int addUser(User user) {
-		return add(Document.parse(user.toJson()), userCollection);
-	}
-
-	@Override
-	public boolean setPlantMonitor(PlantMonitor monitor) {
-		return false;
-	}
-
-	@Override
-	public Plant getPlant(int id) {
-		System.out.println("Database: getplant " + id);
-		Bson filter = new BasicDBObject("PlantId", id);
-		Document document = plantCollection.find(filter).first();
-//		return Plant.fromJson(document.toJson());
-		//TODO delete below
-		Plant p = new Plant();
-		p.id = 1;
-		p.assignedProfile = 2;
-		p.name = "Flower";
-		return p;
-	}
-
-	@Override
-	public boolean setPlant(Plant plant) {
-		return set(new BasicDBObject("PlantId", plant.id), Document.parse(plant.toJson()), plantCollection);
-	}
-
-	@Override
-	public int addPlant(Plant plant) {
-		return add(Document.parse(plant.toJson()), plantCollection);
-	}
-
-	private int add(Document document, MongoCollection<Document> collection) {
-		document.remove("id");
-		try {
-			collection.insertOne(document);
-			return 0;
-		} catch(MongoWriteException e) {
-			return 1;
-		} catch(MongoWriteConcernException f) {
-			return 2;
-		} catch(MongoException g) {
-			return 3;
-		}
-	}
-
-	private boolean set(Bson filter, Document update, MongoCollection<Document> collection) {
-		try {
-			collection.findOneAndReplace(filter, update);
-		} catch(Exception e) {
+	public boolean setAccount(Account account) {
+		Bson filter = new BasicDBObject("Username", account.Username);
+		Document rep = Document.parse(account.toJson());
+		Document res = accountCollection.findOneAndReplace(filter, rep);
+		if(res == null) {
 			return false;
 		}
 		return true;
+	}
+
+	@Override
+	public boolean addAccount(Account user) {
+		try {
+			Document doc = Document.parse(user.toJson());
+			accountCollection.insertOne(doc);
+		} catch(Exception e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean setPlantProfile(PlantProfile monitor) {
+		Bson filter = new BasicDBObject("PlantID", monitor.PlantID);
+		Document rep = Document.parse(monitor.toJson());
+		Document res = plantCollection.findOneAndReplace(filter, rep);
+		if(res == null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public PlantProfile getPlantProfile(int id) {
+		Bson filter = new BasicDBObject("PlantID", id);
+		Document doc = plantCollection.find(filter).first();
+		if(doc == null) {
+			System.out.println("null--");
+			return null;
+		}
+		doc.remove("_id");
+		doc.remove("DateTime");
+		return PlantProfile.fromJson(doc.toJson());
 	}
 }
