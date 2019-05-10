261daa1ec26b71396d2945a5317d10397acb78f4
kenneth <kennethpete@gmail.com>
2019-05-10 16:36:05 +0200

    Listener for sensor websocket

---
+++ src/presenter/EmbeddedListener.java
@@ -0,0 +1,52 @@
+package presenter;
+
+import model.PlantProfile;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import static java.lang.System.exit;
+
+public class EmbeddedListener implements Runnable{
+	WebSocketListener wsl;
+	IDatabaseHandler databaseHandler;
+	String name;
+	int id;
+
+	public EmbeddedListener(IDatabaseHandler databaseHandler) {
+		wsl = new WebSocketListener();
+		this.databaseHandler = databaseHandler;
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(1000);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+				exit(1);
+			}
+			String update = wsl.getMessage();
+			if(update != null) {
+				char[] hex = Document.parse(update).getString("data").toCharArray();
+				int hum = Integer.parseInt("" + hex[0] + hex[1], 16);
+				int temp = Integer.parseInt("" + hex[2] + hex[3], 16);
+				int co2 = Integer.parseInt("" + hex[4] + hex[5] + hex[6] + hex[7], 16);
+				int light = Integer.parseInt("" + hex[8] + hex[9], 16);
+				int water = Integer.parseInt("" + hex[10] + hex[11], 16);
+
+				PlantProfile pp = new PlantProfile();
+				pp.AmountOfWater = 0;
+				pp.CO2 = co2;
+				pp.Humidity = hum;
+				pp.HoursSinceWatering = water;
+				pp.Light = light;
+				pp.Temperature = temp;
+				pp.PlantName = name;
+				pp.PlantID = id;
+
+				//TODO submit to database
+			}
+		}
+	}
+}
---
+++ src/presenter/WebSocketListener.java
@@ -0,0 +1,69 @@
+package presenter;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.WebSocket;
+import java.nio.ByteBuffer;
+import java.util.ArrayDeque;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+public class WebSocketListener implements WebSocket.Listener {
+	public static String STATUS = "WebSocket is closed";
+
+	private ArrayDeque<String> queue;
+
+	public WebSocketListener() {
+		queue = new ArrayDeque<>();
+		CompletableFuture<WebSocket> future = HttpClient.newHttpClient().newWebSocketBuilder()
+				.buildAsync(URI.create(
+						"wss://iotnet.teracom.dk/app?token=vnoRoAAAABFpb3RuZXQudGVyYWNvbS5ka4HekOv-rH_6NfnfbzGsjGY="),
+						this);
+	}
+
+	public String getMessage() {
+		return queue.poll();
+	}
+
+	@Override
+	public void onOpen(WebSocket webSocket) {
+		webSocket.request(1);
+		STATUS = "WebSocket is open";
+	}
+
+	@Override
+	public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+		webSocket.request(1);
+		queue.add(data.toString());
+		return null;
+	}
+
+	@Override
+	public CompletionStage<?> onPing(WebSocket webSocket, ByteBuffer message) {
+		webSocket.request(1);
+		System.out.println("Ping");
+		System.out.println(message.asCharBuffer().toString());
+		return null;
+	}
+
+	@Override
+	public CompletionStage<?> onPong(WebSocket webSocket, ByteBuffer message) {
+		webSocket.request(1);
+		System.out.println("Pong");
+		System.out.println(message.asCharBuffer().toString());
+		return null;
+	}
+
+	@Override
+	public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+		STATUS = "WebSocket closed";
+		return null;
+	}
+
+	@Override
+	public void onError(WebSocket webSocket, Throwable error) {
+		STATUS = "WebSocket aborted";
+		System.err.println("Embedded WebSocket crashed");
+		webSocket.abort();
+	}
+}
